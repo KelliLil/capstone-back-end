147ad971dba7f41beddb308fc91014fe457ca4f8
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-15 11:07:57 -0600

    sloppy commit

--- app/cuisine/controller.js
+++ app/cuisine/controller.js
@@ -2,7 +2,7 @@ import client from "../client.js";
 
 const controller = {
   index() {
-    return client.cuisine.findMany({ select: { cuisine: true } });
+    return client.cuisine.findMany({ select: { name: true } });
   },
 };
 
